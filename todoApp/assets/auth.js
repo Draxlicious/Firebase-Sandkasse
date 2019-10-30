4def0c9c265136f378f7198454e9404a119fa03b
Lukas Jakobsen <109243@RTS-365.dk>
2019-10-30 09:33:09 +0100

    create user fullname + signin to write comment

--- todoApp/assets/auth.js
+++ todoApp/assets/auth.js
@@ -2,31 +2,31 @@ const loginform = document.querySelector('#loginform');
 
 // login på bruger
 loginform.addEventListener('submit', function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.querySelector('#loginform_error').textContent = '';
+  document.querySelector('#loginform_error').textContent = '';
 
-    const email = loginform.username.value;
-    const password = loginform.password.value;
+  const email = loginform.username.value;
+  const password = loginform.password.value;
 
-    // HUSK VALIDERING !!!
+  // HUSK VALIDERING !!!
 
-    auth.signInWithEmailAndPassword(email, password)
-        .then(function (cred) {
-            console.log(cred.user);
-            loginform.reset(); // ryd loginformen
-        })
-        .catch(function (error) {
-            document.querySelector('#loginform_error').textContent = error.message
-        })
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function (cred) {
+      console.log(cred.user);
+      loginform.reset(); // ryd loginformen
+    })
+    .catch(function (error) {
+      document.querySelector('#loginform_error').textContent = error.message
+    })
 });
 
 // log ud af bruger
 const logout_button = document.querySelector("#logout_button");
 logout_button.addEventListener("click", function () {
-    auth.signOut().then(function () {
-        console.log("brugeren er logget af");
-    });
+  auth.signOut().then(function () {
+    console.log("brugeren er logget af");
+  });
 });
 
 // firebase.auth().signOut()
@@ -34,69 +34,70 @@ logout_button.addEventListener("click", function () {
 // opret bruger
 const signupform = document.querySelector('#signupform')
 signupform.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.querySelector("#signinform_error").textContent = "";
+  document.querySelector("#signinform_error").textContent = "";
 
-    const email = signupform.username.value;
-    const password = signupform.password.value;
+  const email = signupform.username.value;
+  const password = signupform.password.value;
 
-    // if (repassword !== password) {
-    //     password.style.backgroundColor = "orange"
-    // }
+  // if (repassword !== password) {
+  //     password.style.backgroundColor = "orange"
+  // }
 
-    // HUSK VALIDERING, og der burde nok være et "GENTAG PASSWORD" felt!
+  // HUSK VALIDERING, og der burde nok være et "GENTAG PASSWORD" felt!
 
-    auth.createUserWithEmailAndPassword(email, password)
-        .then(function (cred) {
-            console.log(cred);
-            signupform.reset();
-        })
-        .catch(function (error) {
-            document.querySelector("#signinform_error").textContent = error.message;
-        });
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(function (cred) {
+      console.log(cred);
+      // tilføj en return handling, som giver os muligheden for at "chaine" .then()
+      return db.collection('users').doc(cred.user.uid).set({
+        fullname: signupform.fullname.value
+      })
+    })
+    .then(function () {
+      signupform.reset();
+    })
+    .catch(function (error) {
+      document.querySelector('#signinform_error').textContent = error.message;
+    });
+
 });
 
 
-auth.onAuthStateChanged(function(user) {
-    if (user != null) {
-      db.collection("todos").onSnapshot(
-        function(snapshot) {
-          let changes = snapshot.docChanges();
-          changes.forEach(function(change) {
-            if (change.type == "added") {
-              renderTodo(change.doc);
-            } else if (change.type == "removed") {
-              let li = todos.querySelector(`li[data-id="${change.doc.id}"]`);
-              todos.removeChild(li);
-            }
-          });
-        },
-        function(error) {
-            console.log(error.message);
-            //   Missing or insufficient permissions.
-        }
-      );
-  
-      // vis alle elementer der skal være tilgængelige efter login
-      // skjul alle elementer der ikke er relevante for en bruger
-    } else {
-      // fjern alle elementer i TODO listen
-      // skjul elementer der kræver man er logget på
-    }
-  });
+auth.onAuthStateChanged(function (user) {
+  if (user != null) {
+    db.collection("todos").onSnapshot(
+      function (snapshot) {
+        let changes = snapshot.docChanges();
+        changes.forEach(function (change) {
+          if (change.type == "added") {
+            renderTodo(change.doc);
+          } else if (change.type == "removed") {
+            let li = todos.querySelector(`li[data-id="${change.doc.id}"]`);
+            todos.removeChild(li);
+          }
+        });
+        db.collection('users').doc(user.uid).get().then(function (doc) {
+          // hvis der er et dokument, udskrives det, indsæt det i et html element
+          if (doc) {
+            console.log(doc.data().fullname);
+          }
+        });
+      },
+      function (error) {
+        console.log(error.message);
+        //   Missing or insufficient permissions.
+      }
+    );
 
-  auth.createUserWithEmailAndPassword(email, password)
-  .then(function (cred) {
-     console.log(cred);
-     // tilføj en return handling, som giver os muligheden for at "chaine" .then()
-     return db.collection('users').doc(cred.user.uid).set({
-        fullname: signupform.fullname.value
-     })
-  })
-  .then(function () {
-     signupform.reset();
-  })
-  .catch(function (error) {
-     document.querySelector('#signinform_error').textContent = error.message;
-  });
+    // vis alle elementer der skal være tilgængelige efter login
+    // skjul alle elementer der ikke er relevante for en bruger
+  } else {
+    // fjern alle elementer i TODO listen
+    // skjul elementer der kræver man er logget på
+  }
+
+});
+
+
