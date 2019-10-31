5ac12bc08ef5b3851c34fd68cc716f0e11f25b02
Lukas Jakobsen <109243@RTS-365.dk>
2019-10-31 10:07:42 +0100

    bolcher til database

---
+++ todoApp/assets/bolcher.js
@@ -0,0 +1,14 @@
+// send data til firebase fra JS
+let bolcheform = document.querySelector('#bolche-form');
+
+bolcheform.addEventListener('submit', function(){
+preventDefault()
+
+db.collection("bolcher-test").add({
+
+    navn : form.input.navn.value
+
+})
+
+
+})
